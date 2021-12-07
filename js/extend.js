ea26e91c45821b0c95e4a16a99eb1c0b595b9d78
SHREYASH DUBEY <askshreyash@gmail.com>
2021-12-07 07:44:39 +0530

    Update extend.js

--- js/extend.js
+++ js/extend.js
@@ -1,7 +1,7 @@
 function openInGithub()
 {
     var a = document.createElement("a");
-    a.href = "https://github.com/SouvikDas-git/AssignmentMaker";
+    a.href = "https://github.com/ishre/Assignment-Maker";
     a.target = "_blank";
     a.click();
     a.remove();
